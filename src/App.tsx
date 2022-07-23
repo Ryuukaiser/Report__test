3e2922fb220b0e9b3fce81e19b5eb717e79977bc
RyuuKaiser <gauvu107@gmail.com>
2022-07-23 21:19:28 +0700

    sua loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,14 @@ import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
 import { useAppDispatch, useAppSelector } from 'app/store/hooks'
 import Home from './pages/Home/Home';
 import Report from './pages/Report/Report';
-import LoadingScreen from './pages/Loading/Loading'
 
 
 
 function App() {
-  const LoadingTime = useAppSelector(state=> state.Loading) 
-  const [loading, setLoading] = useState(LoadingTime.Loading) 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 2000)
-  }, [])
+ 
  
   return (
-    <>
-    { loading === false ? (
+  
     <div className="App">
      <Router>
        <Routes>
@@ -25,10 +19,7 @@ function App() {
        </Routes>
      </Router>
     </div>
-    ) : (
-      <LoadingScreen />
-    )}
-    </>
+    
   );
 }
 
